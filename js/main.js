4ec1a4d46d5aae0fa34bbdfa4c4221c50002a64b
Andrey <andrey.yarovoy.2013@gmail.com>
2022-08-19 15:32:15 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -37,11 +37,9 @@ $(function () {
         nextArrow: false,
     });
 
-    $(function() {
-        $('.news__wrapper').pagination({
-            items: 100,
-            itemsOnPage: 10,
-            cssStyle: 'light-theme'
-        });
+    $('.pagination').pagination({
+        items: 100,
+        itemsOnPage: 10,
+        cssStyle: 'light-theme'
     });
 });
