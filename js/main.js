32fae2c3d0dc3e313b215707fe4bbd6db90f96e1
Andrey <andrey.yarovoy.2013@gmail.com>
2022-08-25 13:37:54 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -17,31 +17,6 @@ $(function () {
         $("#table-sort").tablesorter();
     }); 
 
-    $('.table-filters input').on('input', function () {
-        filterTable($(this).parents('table'));
-    });
-    
-    function filterTable($table) {
-        var $filters = $table.find('.table-filters td');
-        var $rows = $table.find('.table-data');
-        $rows.each(function (rowIndex) {
-            var valid = true;
-            $(this).find('td').each(function (colIndex) {
-                if ($filters.eq(colIndex).find('input').val()) {
-                    if ($(this).html().toLowerCase().indexOf(
-                    $filters.eq(colIndex).find('input').val().toLowerCase()) == -1) {
-                        valid = valid && false;
-                    }
-                }
-            });
-            if (valid === true) {
-                $(this).css('display', '');
-            } else {
-                $(this).css('display', 'none');
-            }
-        });
-    }
-
     $(document).ready(function () {
         $('.filter-list__btn').click(function () {
             var input = document.querySelector('.input-filter');
