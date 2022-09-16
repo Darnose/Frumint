df8b5725f78a701284877c44819c8d5932665999
Andrey <andrey.yarovoy.2013@gmail.com>
2022-09-16 11:31:02 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -42,30 +42,43 @@ $(function () {
         });
     });
 
-    // $(document).ready(function() {
-    //     $(".slider").each(function () { // обрабатываем каждый слайдер
-    //      var obj = $(this);
-    //      $(obj).append("<div class='nav'></div>");
-    //      $(obj).find("li").each(function () {
-    //       $(obj).find(".nav").append("<span rel='"+$(this).index()+"'></span>"); // добавляем блок навигации
-    //       $(this).addClass("slider"+$(this).index());
-    //      });
-    //      $(obj).find("span").first().addClass("on"); // делаем активным первый элемент меню
-    //     });
-    //    });
-    //    function sliderJS (obj, sl) { // slider function
-    //     var ul = $(sl).find("ul"); // находим блок
-    //     var bl = $(sl).find("li.slider"+obj); // находим любой из элементов блока
-    //     var step = $(bl).height(); // ширина объекта
-    //     $(ul).animate({marginTop: "-"+step*obj}, 500); // 500 это скорость перемотки
-    //    }
-    //    $(document).on("click", ".slider .nav span", function() { // slider click navigate
-    //     var sl = $(this).closest(".slider"); // находим, в каком блоке был клик
-    //     $(sl).find("span").removeClass("on"); // убираем активный элемент
-    //     $(this).addClass("on"); // делаем активным текущий
-    //     var obj = $(this).attr("rel"); // узнаем его номер
-    //     sliderJS(obj, sl); // слайдим
-    //     return false;
-    //    });
+    $(document).ready(function () {
+        $('.choose__picture').click(function () {
+            $(this).toggleClass('choose');
+        });
+    });
+
+    $gl.slick({
+        rows: 0,
+        slidesToShow: 2,
+        arrows: false,
+        draggable: false,
+        useTransform: false,
+        mobileFirst: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 1023,
+            settings: {
+              slidesToShow: 1,
+              vertical: true
+            }
+          }
+        ]
+      });
+       
+      $gl2.slick({
+        rows: 0,
+        useTransform: false,
+        prevArrow: ".arrow-left",
+        nextArrow: ".arrow-right",
+        fade: true,
+        asNavFor: $gl
+      });
 
 });
