1016231439c0cf0e313b51bb636181fc4f95654e
Andrey <andrey.yarovoy.2013@gmail.com>
2022-09-22 16:44:06 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -78,4 +78,14 @@ $(function () {
           }
         });
       });
+
+      $(document).ready(function() {
+        $('.popup-youtube').magnificPopup({
+          type: 'iframe',
+          mainClass: 'mfp-fade',
+          removalDelay: 160,
+          preloader: false,
+          fixedContentPos: true
+        });
+      });
 });
